570e9cafb576bf43a359471837a79a82654f3ee5
Brendan Poree <57239582+Blindsyde@users.noreply.github.com>
2019-12-16 16:54:48 -0800

    DogYear.js

---
+++ DogYears.js
@@ -0,0 +1,24 @@
+//my age
+const myAge = 35
+
+//earlyYears variable
+let earlyYears = 2
+
+//calculations
+earlyYears = earlyYears * 10.5
+
+laterYears = myAge - 2
+
+//calculations for dog years
+dogYears = laterYears * 4
+
+console.log(laterYears)
+console.log(dogYears)
+
+//my age in dog years
+myAgeInDogYears = earlyYears + laterYears
+
+myName = 'Brendan'.toLowerCase();
+
+//output statement
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
